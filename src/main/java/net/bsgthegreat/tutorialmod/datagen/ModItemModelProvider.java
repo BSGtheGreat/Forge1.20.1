cc792a05d46ca511ee2fd62233877d152bc15f15
BSGtheGreat <bsgthegreat@gmail.com>
2024-02-29 19:55:05 -0600

    Updated Tutorial

---
+++ src/main/java/net/bsgthegreat/tutorialmod/datagen/ModItemModelProvider.java
@@ -0,0 +1,36 @@
+package net.bsgthegreat.tutorialmod.datagen;
+
+import net.bsgthegreat.tutorialmod.TutorialMod;
+import net.bsgthegreat.tutorialmod.item.ModItems;
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItemModelProvider extends ItemModelProvider {
+
+    public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, TutorialMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        simpleItem(ModItems.SAPPHIRE);
+        simpleItem(ModItems.RAW_SAPPHIRE);
+
+        simpleItem(ModItems.STRAWBERRY);
+        simpleItem(ModItems.METAL_DETECTOR);
+        simpleItem(ModItems.PINE_CONE);
+
+    }
+
+    private ItemModelBuilder simpleItem(RegistryObject<Item> item) {
+        return withExistingParent(item.getId().getPath(),
+                new ResourceLocation("item/generated")).texture("layer0",
+                new ResourceLocation(TutorialMod.MOD_ID, "item/" + item.getId().getPath()));
+    }
+
+}
